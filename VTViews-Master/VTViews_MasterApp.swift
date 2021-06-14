4bcf140d66faaa5fcd4d670c6fb28706fe2099ad
Esben Viskum <viskum@viskum.dk>
2021-06-14 14:14:00 +0200

    Initial Commit

---
+++ VTViews-Master/VTViews_MasterApp.swift
@@ -0,0 +1,17 @@
+//
+//  VTViews_MasterApp.swift
+//  VTViews-Master
+//
+//  Created by Esben Viskum on 14/06/2021.
+//
+
+import SwiftUI
+
+@main
+struct VTViews_MasterApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
